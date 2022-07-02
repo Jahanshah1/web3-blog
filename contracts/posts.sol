c243aa52692e3b72389e82cc3c6c66218bd2a451
Jahan Shah <92823408+Jahanshah1@users.noreply.github.com>
2022-07-02 21:00:51 +0530

    Add files via upload

---
+++ contracts/posts.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract PostList {
+    uint public postCount = 0;
+
+    struct Post {
+        uint id;
+        string author;
+        string body;
+    }
+
+    mapping(uint => Post) public posts;
+    
+
+    function createPost(string memory _author, string memory _body) public {
+        postCount ++;
+        posts[postCount] = Post(postCount, _author, _body);
+    }
+
+    function getPostCount() public view returns (uint) {
+        return postCount;
+    }
+
+    function getPosts() public view returns (Post[] memory) {
+        Post[] memory list = new Post[](postCount);
+        for(uint i = 0; i < postCount; i++) {
+            list[i] = posts[i];
+        }
+        return list;
+    }
+}
